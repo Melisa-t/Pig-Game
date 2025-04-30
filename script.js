deb88ae2bd799b7ea2214b4f0ffa24fad0db6477
Melisa Lachev <melisatormac55@gmail.com>
2025-04-30 19:35:02 +0300

    added getting dice as a method

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // 2 players, turn based.
 
 const firstPlayer = {
-  totalScore: 0,
+  totalScore: [],
+  currentScore: 0   ,
+  isPlayersTurn: true,
+
+getDice: function () {
+    this.currentScore = Math.trunc(Math.random() * 6 + 1);
+    for (let i = 0; i < this.currentScore; i++) {
+        const dice = document.querySelector(`.dice`)
+        const diceDiv = document.createElement("div");
+        dice.appendChild(diceDiv);
+    }
+    console.log(this.currentScore)
+  }
+};
+
+const secondPlayer = {
+  totalScore: [],
   currentScore: 0,
   isPlayersTurn: false,
 };
 
-const secondPlayer = {
-  totalScore: 0,
-  currentScore: 0,
-  isPlayersTurn: true,
-};
-
+firstPlayer.getDice()
