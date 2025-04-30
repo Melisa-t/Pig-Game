deba4c4f6bf585b68d05a41a4dc1da47d764e1a9
Melisa Lachev <melisatormac55@gmail.com>
2025-04-30 19:09:18 +0300

    created players as objects

---
+++ script.js
@@ -0,0 +1,14 @@
+// 2 players, turn based.
+
+const firstPlayer = {
+  totalScore: 0,
+  currentScore: 0,
+  isPlayersTurn: false,
+};
+
+const secondPlayer = {
+  totalScore: 0,
+  currentScore: 0,
+  isPlayersTurn: true,
+};
+
