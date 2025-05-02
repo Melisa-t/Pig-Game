229d5c0c033172ac4a891558647cac36501ce3eb
Melisa Lachev <melisatormac55@gmail.com>
2025-05-02 18:02:27 +0300

    hid mid container in case of a winner and only left new game shown

--- script.js
+++ script.js
@@ -7,22 +7,44 @@ const game = {
     return !this.isPlayersTurn ? this.playerOne : this.playerTwo;
   },
 
+  createDice: function (score) {
+    const dice = document.querySelector(`.dice`);
+    //i need to reset dice every time except the first time
+    // when roll is called. if children none dont call the method.
+    // reset dice.
+    
+    // if (dice.hasChildNodes()) {
+    //   console.log(score);
+    //   console.log(dice.childNodes);
+    //   for (let i = 0; i <= score; i++) {
+    //     console.log(
+    //       `if statement true. child siliniyor` + dice.children.length
+    //     );
+    //     console.log();
+    //     dice.childNodes[i].remove();
+    //   }
+    //   for (let i = 0; i < score; i++) {
+    //     const diceDiv = document.createElement("div");
+    //     dice.appendChild(diceDiv);
+    //     console.log(
+    //       `if statement true. child yaratiliyor` + dice.children.length
+    //     );
+    //   }
+    // } else {
+    //   for (let i = 0; i < score; i++) {
+    //     const diceDiv = document.createElement("div");
+    //     dice.appendChild(diceDiv);
+    //     console.log(
+    //       `if statement false. child yaratiliyor` + dice.children.length
+    //     );
+    //   }
+    // }
+  },
   roll: function () {
     const currentPlayer = this.getCurrentPlayer();
     const currentScore = Math.trunc(Math.random() * 6 + 1);
     console.log("Before change", currentScore);
-
-    // const dice = document.querySelector(`.dice`);
-    // console.log(dice.children);
-
-    //i need to reset dice every time except the first time
-    // when roll is called. if children none dont call the method. reset dice.
-
-    // for (let i = 0; i < currentScore; i++) {
-    //   const diceDiv = document.createElement("div");
-    //   dice.appendChild(diceDiv);
-    // }
-
+    // game.createDice(currentScore);
     if (currentScore === 1) {
       currentPlayer.score = 0;
       this.isPlayersTurn = !this.isPlayersTurn;
@@ -50,20 +72,25 @@ const game = {
   },
 
   reset: function () {
+    const midContainer = document.querySelector(`.mid-container`)
     this.playerOne.totalScore = 0;
     this.playerTwo.totalScore = 0;
     this.playerOne.score = 0;
     this.playerTwo.score = 0;
     this.isPlayersTurn = false;
+    midContainer.style.display = `flex`
   },
 
   winCheck: function () {
+    const midContainer = document.querySelector(`.mid-container`)
     const currentPlayer = this.getCurrentPlayer();
     const currentTotalScore = currentPlayer.totalScore + currentPlayer.score;
     if (currentTotalScore >= 12) {
       this.winner = currentPlayer;
       !currentPlayer ? `player1` : `player2`;
       console.log(`${!currentPlayer ? `player1` : `player2`}`);
+      midContainer.style.display = `none`
+      resetBtn.style.display=`flex`
     }
   },
 
